19716b6e91e174463faff51e0d99e84366ce3f9a
Sunil Chauhan <CSunil11@users.noreply.github.com>
2021-07-21 13:11:47 +0000

    Adding Tomcat dependencies

--- src/main/java/com/exam/config/DemoApplication.java
+++ src/main/java/com/exam/config/DemoApplication.java
@@ -3,11 +3,24 @@ package com.exam.config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+    System.out.println("DemoApplication started");
 	}
+}
 
+@RestController
+class GreetingController {
+    
+    @RequestMapping("/hello/{name}")
+    String hello(@PathVariable String name) {
+        return "Hello, " + name + "!" + ;
+    }
 }
